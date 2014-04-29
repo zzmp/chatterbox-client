6ff68f2f824ce87d4c2809e02933f608f51d973e
zzmp <zmp@umich.edu>
2014-04-28 20:38:02 -0700

    Finished Basic Requirements.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,19 @@
 // YOUR CODE HERE:
 
 (function() {
+
+  // Initial condtion for time filter
+  var lastLoaded = "0000-01-01T00:00:00.000Z";
+
   var messageToHtml = function (message) {
+    message.roomname = decodeURI(message.roomname);
+    message.username = decodeURI(message.username);
+    message.text = decodeURI(message.text);
+
     var li = $('<li></li>')
-      .data('room' , message.roomname) // unsanitary
-      .data('user' , message.username);  // unsanitary
+      .addClass('chat')
+      .data('room' , message.roomname)
+      .data('user' , message.username);
 
     var text = $('<div></div>')
       .addClass('message')
@@ -15,7 +24,13 @@
       .text(message.username)
       .on('click', app.addFriend);
 
-    li.append(text).append(user);
+    li.append(user).append(text);
+
+    if (selectedRoom && message.roomname !== selectedRoom) {
+      li.hide();
+    }
+
+    app.addRoom(message.roomname);
 
     return li;
   };
@@ -25,7 +40,7 @@
   app.init = function () {};
 
   app.send = function (message) {
-    $.ajax({
+    $.ajax(undefined, {
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'POST',
       data: JSON.stringify(message),
@@ -34,10 +49,35 @@
   };
 
   app.fetch = function () {
+    // stringify the time filter request by building sort/time separately
+    var sortFilter = 'order=-updatedAt';
+    sortFilter = encodeURI(sortFilter);
+
+    var timeFilter = {
+      updatedAt: {
+        $gt:  {
+          __type: "Date",
+          iso: lastLoaded
+        }
+      }
+    };
+    timeFilter = 'where=' + JSON.stringify(timeFilter);
+
+    var filter = sortFilter + '&' + timeFilter;
+
     $.ajax(undefined, {
       url: 'https://api.parse.com/1/classes/chatterbox',
       type: 'GET',
-      // success: app.addMessage
+      data: filter,
+      success: function (data) {
+        for (var i = data.results.length - 1; i >= 0; i--) {
+          app.addMessage(data.results[i]);
+        }
+
+        if (data.results.length) {
+          lastLoaded = data.results[0].updatedAt;
+        }
+      }
     });
   };
 
@@ -46,39 +86,83 @@
   };
 
   app.addMessage = function (message) {
-    $('#chats').append(messageToHtml(message));
-    // app.addRoom(message.roomname); // unsanitary
+    $('#chats').prepend(messageToHtml(message));
   };
 
   app.addRoom= function (room) {
-    if (!$("#roomSelect option[value='" + room + "']").length) {
-      var option = $('<option></option>')
-        .attr('value', room)
-        .text(room);
+    if (room) {
+      if (!$("#roomSelect option[value='" + room + "']").length) {
+        var option = $('<option></option>')
+          .attr('value', room)
+          .text(room);
 
-      $('#roomSelect').append(option);
+        $('#roomSelect').append(option);
+      }
     }
   };
 
-  app.addFriend = function (user) {
-    // TODO: add friend on click
+  app.addFriend = function (e) {
+    var user = $(e.target).parent().data('user');
+
+    $('#chats').children().each(function () {
+      if ($(this).data('user') === user) {
+        $(this).css('font-weight', 'bold');
+      }
+    });
+  };
+
+  var findName = function () {
+    var input = window.location.search;
+    var start = input.search(/username\=/);
+    var end = input.substr(start).search(/\&/);
+    return end === -1 ?
+      input.substr(start+9) : // 9 is the length of username=
+      input.substring(start+9, end+start);
   };
 
   app.handleSubmit = function() {
     app.send({
-      username: 'zz', // TODO: get window.location.search
-      text: $('input #message').val(), //unsanitary
-      roomname: 'zz'
+      username: findName(), // TODO: get window.location.search
+      text: encodeURI($('input#message').val()),
+      roomname: encodeURI($('input#roomText').val())
     }); // unsanitary
   };
+
+  var selectedRoom = null;
+
+  app.selectRoom = function(e) {
+    var room = $(this).val();
+
+    if (room === 'All Rooms') {
+      selectedRoom = null;
+      $('#chats').children().show();
+    } else {
+      selectedRoom = room;
+      $('#chats').children().each(function () {
+        if ($(this).data('room') === room) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+    }
+  }
 
   this.app = app;
 }() );
 
 $(document).ready(function(){
   var submit = $('#send');
+  var room = $('#roomSelect');
 
-  submit.on('submit', function() {
+  submit.on('submit', function(e) {
     app.handleSubmit();
+    e.preventDefault();
+    e.stopPropagation();
   });
+
+  room.on('change', app.selectRoom);
+
+  app.fetch();
+  setInterval(function(){app.fetch();},1000);
 });
